d0af3d83d227168c1ffc9a25c5c5b1c2cd971cc1
Jesus Arias <ariasje@oregonstate.edu>
2024-08-30 23:37:00 -0500

    Update Fibonacci-Function.py

--- Fibonacci-Function.py
+++ Fibonacci-Function.py
@@ -10,13 +10,13 @@ def fib(number):
     fib_number = 1
     for n in range(1, number + 1):
         if n == 1 or n == 2:
-            fib_number = 1
-        elif n == 3:
-            fib_number = first_number + second_number
+            fib_number = 1  # The first two Fibonacci numbers are always 1
         else:
+            # Calculate the next Fibonacci number
+            fib_number = first_number + second_number
+            # Update the two preceding numbers for the next iteration
             first_number = second_number
             second_number = fib_number
-            fib_number = first_number + second_number
     return fib_number
 
 
